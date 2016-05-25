04ae74ea7e9f6f1808351fb85c094b177c1ffcc4
ShadowTemplate <taneburgo@gmail.com>
2016-05-25 14:36:06 +0200

    It wasn't really hard indeed...

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import logging
-import time
 import webapp2
+import one_piece_bot
+from timeit import default_timer as timer
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(one_piece_bot.get_status())
 
 
 class ReleaseHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Get')
-        logging.info('Get request at ' + str(time.strftime("%c")))
-        logging.info(self.request)
+        start_time = timer()
+        one_piece_bot.check_releases()
+        self.response.write("Request completed in " + str((timer() - start_time)) + " seconds.")
 
 
 app = webapp2.WSGIApplication([
